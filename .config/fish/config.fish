10dacc264072965fcce20b9432374e2d37886511
IgnacyBerent <2gb02ignac@gmail.com>
2025-10-20 00:18:41 +0200

    -m

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,8 +16,7 @@ alias tree="lsd --tree"
 alias grep="rg"
 alias okular="okular --stylesheet /home/iberent/.config/okular.qss"
 
-alias discord="flatpak run com.discordapp.Discord"
-
 zoxide init --cmd cd fish | source
 fzf --fish | source
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+oh-my-posh init fish --config ~/.poshthemes/montys.omp.json | source
