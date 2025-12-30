5769562976d3fc40c6301ceb870f2d5a17efb687
IgnacyBerent <2gb02ignac@gmail.com>
2025-12-30 16:51:00 +0100

    Setting the qt apps theme using kvantum and qt6ct

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ set -gx VISUAL nvim
 set -gx TERMINAL ghostly
 set -gx PATH /opt/android-sdk/platform-tools $PATH
 set -gx JAVA_HOME /usr/lib/jvm/java-21-openjdk
+set -gx QT_QPA_PLATFORMTHEME qt6ct
 
 fish_add_path $HOME/.local/bin
 
@@ -20,7 +21,6 @@ alias la="ls -a"
 alias ll="lsd -alh"
 alias tree="lsd --tree"
 alias grep="rg"
-alias okular="okular --stylesheet /home/iberent/.config/okular.qss"
 alias docker="sudo docker"
 
 # Tools init
